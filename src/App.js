596e9c8090b87146842ec073c9319ae68399ad08
Barkley Santo <barkley.santo@gmail.com>
2020-12-30 12:46:15 -0600

    comments to know where other components will be nested

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 class App extends Component {
   render() {
     return (
+      /* We will nest the rest of our components in this div also */
       <div className="App">
         <h1>Hi, I'm a React App</h1>
       </div>
--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+// <App/> is a exported from App.js
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
