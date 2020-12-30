57049cad295a70fdefa4e52e221d074df7eac588
Barkley Santo <barkley.santo@gmail.com>
2020-12-30 17:34:17 -0600

    using react.createElement

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      /* We will nest the rest of our components in this div also */
-      <div className="App">
-        <h1>Hi, I'm a React App</h1>
-      </div>
-    );
+    // return (
+    //   /* We will nest the rest of our components in this div also */
+    //   <div className="App">
+    //     <h1>Hi, I'm a React App</h1>
+    //   </div>
+    // );
+    // h1 is interpreted as text
+    return React.createElement('div', null, React.createElement('h1', null, 'Does this work now?'))
   }
 }
 
