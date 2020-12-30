b4fc30eb3aa2b08f237140d66a950a6486da70f1
Barkley Santo <barkley.santo@gmail.com>
2020-12-30 17:58:07 -0600

    remembering to export the component we created

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,8 @@
 import React from "react"; // must import react in each ocmponent file
 
+// this is the preferred way to write functions.
 const person = () => {
   return <p>I'm a person!</p>
 }
 
-export default person; // exporting the fn
+export default person; // exporting the fn DONT FORGET
