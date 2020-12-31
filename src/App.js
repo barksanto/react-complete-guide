2f5be9ae900fa4c8e8ca4da7dd927e6e7579afd0
Barkley Santo <barkley.santo@gmail.com>
2020-12-31 10:52:14 -0600

    change class based component into functional component

--- src/App.js
+++ src/App.js
@@ -3,44 +3,44 @@ import './App.css';
 // import Person compontnt from Person Folder using Person.js file
 import Person from './Person/Person';
 
-// app is our root ocmponent
-class App extends Component {
-  state = {
-    persons: [
-      { name: "Barkley", age: 27 },
-      { name: "Mango", age: 6 },
-      { name: "Mikayla", age: 20 }
-    ]
-  }
+// app is our root component
+// class App extends Component {
+const App = props => { //no render method on functional components
+  return (
+    /* We will nest the rest of our components in this div also */
+    /* We render by using the same name of the component we imported*/
+    <div className="App">
+      <h1>Hi, I'm a React App</h1>
+      <p>This is my next paragraph</p>
+      <button onClick={this.switcNameHandler}>Switch Name</button>
+      <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>My Hobbies : pizza</Person>
+      <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+      <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
 
-  switcNameHandler = () => { // normal es6 arrow function // must use this syntax so we can use 'this'
-    // console.log('button has been clicked!!');
-    this.setState({
-      persons: [
-        { name: "Bananamamamama", age: 27 },
-        { name: "Mango", age: 6 },
-        { name: "Mikayla", age: 100 }
-      ]
-    })
-  }
-
-  render() {
-    return (
-      /* We will nest the rest of our components in this div also */
-      /* We render by using the same name of the component we imported*/
-      <div className="App">
-        <h1>Hi, I'm a React App</h1>
-        <p>This is my next paragraph</p>
-        <button onClick={this.switcNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>My Hobbies : pizza</Person>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
-
-      </div>
-    );
-    // h1 is interpreted as text - this is the same as above, just much more cumbersome
-    // return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'Does this work now?'))
-  }
+    </div>
+  );
+  // h1 is interpreted as text - this is the same as above, just much more cumbersome
+  // return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'Does this work now?'))
 }
 
 export default App;
+
+
+// state = {
+//   persons: [
+//     { name: "Barkley", age: 27 },
+//     { name: "Mango", age: 6 },
+//     { name: "Mikayla", age: 20 }
+//   ]
+// }
+
+// switcNameHandler = () => { // normal es6 arrow function // must use this syntax so we can use 'this'
+//   // console.log('button has been clicked!!');
+//   this.setState({
+//     persons: [
+//       { name: "Bananamamamama", age: 27 },
+//       { name: "Mango", age: 6 },
+//       { name: "Mikayla", age: 100 }
+//     ]
+//   })
+// }
