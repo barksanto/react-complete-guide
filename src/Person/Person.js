898765f0b7e20b9824a6b5d019423c0543d82cbe
Barkley Santo <barkley.santo@gmail.com>
2021-01-02 09:32:09 -0600

    create stylesheet, import it onto out component.js file

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,15 +1,16 @@
 import React from "react"; // must import react in each component file
+import './Person.css';
 
 // this is the preferred way to write functions.
 const person = (props) => {
   return (
-    <div>
+    <div className="Person">
       {/* click is the name of the property we defined on the My Hobbies */}
       {/* click is the name of the property we dfined in the element in App.js */}
       <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
       {/* This is how we can return what's inside of the <Person></Person> */}
       {/* <p>{props.children}</p> */}
-      <input type="text" onChange={props.changed} />
+      <input type="text" onChange={props.changed} value={props.name} />
     </div>
   )
 }
