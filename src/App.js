a3ca1b2ced25d7d0a09abc44bee47e06027bf226
Barkley Santo <barkley.santo@gmail.com>
2021-01-06 12:32:52 -0600

    refactoring hard coded elements - first mapping over the persons array of objects

--- src/App.js
+++ src/App.js
@@ -62,6 +62,9 @@ class App extends Component { //no render method on functional components
     if (this.state.showPersons) {
       persons = (
         <div>
+          {this.state.persons.map(person => {
+
+          })}
           <Person
             name={this.state.persons[0].name}
             age={this.state.persons[0].age}
