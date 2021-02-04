073097e42e859629feb4d4db5ccc2e86007a5b23
Barkley Santo <barkley.santo@gmail.com>
2021-02-04 14:00:36 -0500

    add componentDid catch error message amd state to modify any output

--- src/ErrorBoundary.js
+++ src/ErrorBoundary.js
@@ -1,7 +1,19 @@
 import React, { Component } from 'react';
 
 class ErrorBoundary extends Component {
+  state = {
+    hasError: false,
+    errorMessage: ''
+  }
+
+  componentDidCatch = (error, info) => {
+    this.setState({ hasError: true, errorMessage: error });
+  }
+
   render() {
-    return <h1>Something went wrong</h1>
+    if (this.state.hasError) {
+      return <h1>Something went wrong</h1>
+    }
+
   }
 }
